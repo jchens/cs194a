6b4a6946648541140079f1986b2f46a4a1bfc77b
Jessica Chen <jchens@stanford.edu>
2020-10-28 01:35:09 -0700

    Removed action bar from main page

---
+++ 1 -Splip/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.assignment1
+
+import android.animation.ArgbEvaluator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.SeekBar
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sbTip.progress = INITIAL_TIP_PERCENT
+        tvTipPercent.text = "$INITIAL_TIP_PERCENT%"
+        updateTipDescription(sbTip.progress)
+
+        sbTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                tvTipPercent.text = "$p1% "
+
+                updateTipDescription(p1)
+                computeTipAndTotal()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+
+        etBase.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                computeTipAndTotal()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+
+        etPeopleAmount.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                computeTipAndTotal()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription: String
+        when (tipPercent) {
+            in 0..9 -> tipDescription = "🤬 poor"
+            in 10..14 -> tipDescription = "😐 acceptable"
+            in 15..19 -> tipDescription = "😬 good"
+            in 20..24 -> tipDescription = "🥳 great"
+            else -> tipDescription = "🤩 amazing"
+        }
+
+        tvTipDescrip.text = tipDescription
+        val color = ArgbEvaluator().evaluate(
+            tipPercent / sbTip.max.toFloat(),
+            ContextCompat.getColor(this, R.color.colorWorstTip),
+            ContextCompat.getColor(this, R.color.colorBestTip)
+        ) as Int
+
+        tvTipDescrip.setTextColor(color)
+
+    }
+
+    private fun computeTipAndTotal() {
+        // guard against empty case
+        if (etBase.text.isBlank()) {
+            tvTipAmount.text = "N/A"
+            tvTotalAmount.text = "N/A"
+            return
+        }
+
+        // get value of base, tip percentage, # of people
+        val base = etBase.text.toString().toDouble()
+        val tipPercent = sbTip.progress
+        var numPeople = 1
+
+        if (etPeopleAmount.text.isBlank()) {
+//            etPeopleAmount.setText("1")
+        } else if (etPeopleAmount.text.toString().toInt() > 0) {
+            numPeople = etPeopleAmount.text.toString().toInt()
+        }
+
+        // calculate vars
+        val tipAmount = tipPercent * base / 100
+        val totalAmount = (tipAmount + base) / numPeople
+
+        // set UI
+        tvTipAmount.text = "$" + "%.2f".format(tipAmount)
+        tvTotalAmount.text = "$" + "%.2f".format(totalAmount)
+
+    }
+}
---
+++ 2 - Let's Explore/README.md
@@ -0,0 +1,59 @@
+# Let's Explore
+
+## Jessica Chen
+
+**Let's Explore** displays a list of maps, each of which show user-defined pins with a title, description, and location. The user can also create a new map.
+
+Time spent: **10** hours spent in total
+
+## Functionality
+
+The following **required** functionality is completed:
+
+* [x] The list of map titles is displayed.
+* [x] After tapping on a map title, the associated pins in the map are shown.
+* [x] The user is able to create a new map.
+
+The following **extensions** are implemented:
+
+* [x] Changed the color scheme of the app
+* [x] In the initial screen, the number of places in each map is shown along with the title.
+* [x] Pins changed from the default icon
+
+## Video Walkthrough
+
+Here's a walkthrough of the implemented maps:
+
+### Viewing your maps
+* See all maps & their place count
+* Tap on a map to view all pinned places
+* Click on pins to view their title & description
+<img src='/MyMaps/existing_map.gif?raw=true' title='Exploring Existing Maps' width='40%' alt='Exploring Existing Maps' />
+
+### Creating a map
+* Customize your map name
+* Long press to add a pin and its title & description
+* Remove a pin by tapping its title/description window
+* Map list updates to include new map
+<img src='/MyMaps/new_map.gif?raw=true' title='Creating a New Map' width='40%' alt='Creating a New Map' />
+
+
+## Notes
+
+The most challenging part of this project was trying to work around the default theme to customize everything. At one point, I tried adding another theme so the action bar wouldn't show up, but ever after multiple Stack Overflow posts, it wasn't working out. Understanding how to convert vector images into Bitmap images also took quite a while, but it worked out in the end!
+
+## License
+
+    Copyright [2020] Jessica Chen
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
